b2f019c894b38bdcc4ec1a51d8830c565a4aef0f
Jnuka <jnuka@mail.ru>
2025-09-07 19:32:48 +0400

    feat: add leaderboard-page

---
+++ src/app/components/leaderboard-page/leaderboard-page.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-leaderboard-page',
+  imports: [],
+  templateUrl: './leaderboard-page.html',
+  styleUrl: './leaderboard-page.scss'
+})
+export class LeaderboardPage {
+
+}
