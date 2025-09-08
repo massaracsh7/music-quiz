0630e5b4880c38ac68db70832b6efc65e98c8eea
tubyliec <antikruk@vivaldi.net>
2025-09-08 23:57:19 +0300

    feat: add waveform

--- src/app/components/home-page/home-page.ts
+++ src/app/components/home-page/home-page.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { Game } from '../game/game';
 
 @Component({
   selector: 'app-home-page',
-  imports: [],
+  imports: [Game],
   templateUrl: './home-page.html',
   styleUrl: './home-page.scss',
 })
