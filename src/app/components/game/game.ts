72328a8b4b4f9cf94d4cc952316fb0d6fad2b4fd
tubyliec <antikruk@vivaldi.net>
2025-09-09 00:46:50 +0300

    refactor: add unused methods

--- src/app/components/game/game.ts
+++ src/app/components/game/game.ts
@@ -1,4 +1,4 @@
-import { Component, effect, ElementRef, signal, ViewChild } from '@angular/core';
+import { Component, effect, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import WaveSurfer from 'wavesurfer.js';
 
