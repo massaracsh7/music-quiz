6e77245df78dd72a00367cd2f8c5ec573c1b33d0
tubyliec <antikruk@vivaldi.net>
2025-09-09 22:31:27 +0300

    refactor: add wavesurfer component

---
+++ src/app/shared/services/wavesurfer/wavesurfer.ts
@@ -0,0 +1,46 @@
+import { Injectable, signal } from '@angular/core';
+import WaveSurfer from 'wavesurfer.js';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class Wavesurfer {
+  private wavesurfer?: WaveSurfer;
+
+  currentTime = signal<number>(0);
+  isPlaying = signal<boolean>(false);
+
+  public init(container: string, songUrl: string): void {
+    if (this.wavesurfer) {
+      this.wavesurfer.destroy();
+    }
+
+    this.wavesurfer = WaveSurfer.create({
+      container,
+      waveColor: '#4F4A85',
+      progressColor: '#383351',
+      dragToSeek: false,
+      interact: false,
+      url: songUrl,
+    });
+
+    this.wavesurfer.on('audioprocess', (currentTime) => {
+      this.currentTime.set(Math.floor(currentTime));
+    });
+
+    this.wavesurfer.on('finish', () => {
+      this.isPlaying.set(false);
+    });
+  }
+
+  public playPause(): void {
+    if (!this.wavesurfer) return;
+    this.wavesurfer.playPause();
+    this.isPlaying.set(this.wavesurfer.isPlaying());
+  }
+
+  public destroy(): void {
+    this.wavesurfer?.destroy();
+    this.wavesurfer = undefined;
+  }
+}
