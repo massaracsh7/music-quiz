6d342f26d2beefe40033f40cd53923d9727df6cf
massaracsh7 <fatum7@gmail.com>
2025-09-10 10:05:42 +0300

    feat: add header

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,10 +2,11 @@ import { Component, signal } from '@angular/core';
 import { RouterModule, RouterOutlet } from '@angular/router';
 import { Toast } from './shared/ui/toast/toast';
 import { Header } from './layout/header/header';
+import { Footer } from './layout/footer/footer';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RouterModule, Toast, Header],
+  imports: [RouterOutlet, RouterModule, Toast, Header, Footer],
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
