1f16594bd140f69eca87aa865087fab5e4b3dba1
massaracsh7 <fatum7@gmail.com>
2025-09-13 19:27:27 +0300

    feat: add theme saving to local storage

--- src/app/shared/services/theme/theme.ts
+++ src/app/shared/services/theme/theme.ts
@@ -2,11 +2,18 @@ import { Injectable, signal } from '@angular/core';
 
 @Injectable()
 export class Theme {
-  theme = signal<'light' | 'dark'>('light');
+theme = signal<'light' | 'dark'>(
+    (localStorage.getItem('theme') as 'light' | 'dark') ?? 'light'
+  );
+
+  constructor() {
+    document.documentElement.setAttribute('data-bs-theme', this.theme());
+  }
 
   setTheme(newTheme: 'light' | 'dark') {
     this.theme.set(newTheme);
     document.documentElement.setAttribute('data-bs-theme', newTheme);
+    localStorage.setItem('theme', newTheme);
   }
 
   toggleTheme() {
