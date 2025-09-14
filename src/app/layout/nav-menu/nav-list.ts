8f9fa610e89d12133fdd7f35c6767f32b2e965eb
massaracsh7 <fatum7@gmail.com>
2025-09-14 14:59:23 +0300

    refactor: update nav-menu

---
+++ src/app/layout/nav-menu/nav-list.ts
@@ -0,0 +1,6 @@
+export const NavList = [
+  { path: '/home', label: 'Home', icon: 'house' },
+  { path: '/game', label: 'Game', icon: 'controller' },
+  { path: '/leaderboard', label: 'Leaderboard', icon: 'trophy' },
+  { path: '/about', label: 'About Us', icon: 'info-circle' }
+];
