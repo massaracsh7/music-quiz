eb0b2ef60a95095291c1887d95f547afa57bfd0c
tubyliec <antikruk@vivaldi.net>
2025-09-17 18:12:44 +0300

    feat: add result modal component

---
+++ src/app/components/result-modal/result-modal.ts
@@ -0,0 +1,15 @@
+import { Component, input, output } from '@angular/core';
+import { Track } from '../../models/types/track.type';
+
+@Component({
+  selector: 'app-result-modal',
+  imports: [],
+  templateUrl: './result-modal.html',
+  styleUrl: './result-modal.scss',
+})
+export class ResultModal {
+  public showResultDialog = input<boolean>();
+  public resultMessage = input<string>();
+  public currentTrack = input<Track>();
+  public closeDialog = output<void>();
+}
