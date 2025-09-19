2f57bf6a08f93494a6665a2dfa28588aa12b6fa3
tubyliec <antikruk@vivaldi.net>
2025-09-20 00:54:58 +0300

    refactor: refactor progress bar

--- src/app/components/modals/result-modal/result-modal.ts
+++ src/app/components/modals/result-modal/result-modal.ts
@@ -1,4 +1,4 @@
-import { Component, input, output, effect } from '@angular/core';
+import { Component, input, output, effect, computed } from '@angular/core';
 import { Track } from '../../../models/types/track.type';
 import { CommonModule } from '@angular/common';
 
@@ -11,8 +11,11 @@ import { CommonModule } from '@angular/common';
 export class ResultModal {
   public showResultDialog = input<boolean>();
   public currentTrack = input<Track>();
+  public currentTrackNumber = input<number>();
+  public totalTracks = input<number>();
   public isCorrectSignal = input<boolean>(false);
   public closeDialog = output<void>();
+  public trackResults = input<boolean[]>([]);
 
   constructor() {
     effect(() => {
