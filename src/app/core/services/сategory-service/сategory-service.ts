8911c9c7e4396807328b0c3e633e8cd63924a6ac
massaracsh7 <fatum7@gmail.com>
2025-09-20 22:17:09 +0300

    feat: add itunes-service for cache tracks

--- src/app/core/services/сategory-service/сategory-service.ts
+++ src/app/core/services/сategory-service/сategory-service.ts
@@ -1,13 +1,15 @@
 import { Injectable, inject, Signal } from '@angular/core';
 import { Firestore, collection, collectionData, doc, setDoc } from '@angular/fire/firestore';
 import { toSignal } from '@angular/core/rxjs-interop';
-import { catchError, from, map, Observable, throwError } from 'rxjs';
+import { catchError, from, map, Observable, switchMap, tap, throwError } from 'rxjs';
 import { Category } from '../../../models/category.model';
 import { LeaderboardCategory } from '../../../models/leaderboard.model';
+import { ItunesService } from '../itunes-service';
 
 @Injectable({ providedIn: 'root' })
 export class CategoryService {
   public firestore = inject(Firestore);
+  public itunesService = inject(ItunesService);
 
   public categories: Signal<Category[]>;
 
@@ -21,12 +23,11 @@ export class CategoryService {
 
   public createCategory(category: Category): Observable<string> {
     const categoryDocumentReference = doc(this.firestore, 'categories', category.id);
-
     return from(
       setDoc(categoryDocumentReference, {
         ...category,
       }),
-    ).pipe(
+    ).pipe(switchMap(() => this.itunesService.cacheTracks(category.tracks || [])),
       map(() => category.id),
       catchError((error) => {
         console.error('Error creating category:', error);
