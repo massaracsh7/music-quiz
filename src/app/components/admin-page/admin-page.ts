f11198cacfb5d36b1e876e143d3ff4dedaf9a0a5
massaracsh7 <fatum7@gmail.com>
2025-09-20 17:42:29 +0300

    feat: add toasts change role to admin-page

--- src/app/components/admin-page/admin-page.ts
+++ src/app/components/admin-page/admin-page.ts
@@ -1,8 +1,11 @@
-import { Component, inject, computed, signal } from '@angular/core';
+import { Component, inject, computed, signal, DestroyRef } from '@angular/core';
 import { UserService } from '../../core/services/user-service/user-service';
 import { AppUser, UserInfo, UserRole } from '../../models/user.model';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { catchError, of, tap } from 'rxjs';
+import { ToastService } from '../../shared/services/toast/toast';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'app-admin-page',
@@ -24,13 +27,23 @@ export class AdminPage {
   public currentUserRole = this.userService.currentUserRole;
   public canChangeRoles = this.userService.canChangeRoles;
   public canCreateCategories = this.userService.canCreateCategories;
+  public toast = inject(ToastService);
+  private destroyRef = inject(DestroyRef);
+
 
   public changeRole(user: UserInfo, newRole: UserRole) {
-    if (this.canChangeRoles()) {
-      this.userService.updateUserRole(user.uid, newRole as UserRole).subscribe();
-    }
+    if (!this.canChangeRoles()) return;
+    this.userService.updateUserRole(user.uid, newRole).pipe(
+      tap(() => {
+        this.toast.show(`Role of "${user.email}" changed successfully`, 'success');
+      }),
+      catchError(err => {
+        this.toast.show(`Failed to change role: ${err.message}`, 'error');
+        return of();
+      }),      
+      takeUntilDestroyed(this.destroyRef)
+    ).subscribe();
   }
-
   public sortDirection = signal<'asc' | 'desc'>('asc');
 
   public sortedUsers = computed(() => {
