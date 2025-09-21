63cca425006a36ab5269a242737748ee5e3b124b
tubyliec <antikruk@vivaldi.net>
2025-09-21 17:56:28 +0300

    refactor: delete unused imports

--- src/app/core/services/language-service/language-service.ts
+++ src/app/core/services/language-service/language-service.ts
@@ -1,4 +1,4 @@
-import { computed, inject, Injectable, signal } from '@angular/core';
+import { inject, Injectable, signal } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 
