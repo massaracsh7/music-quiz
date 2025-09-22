195faf9ea8bbccf22513f38329cb70dafd5f2ce5
massaracsh7 <fatum7@gmail.com>
2025-09-23 01:57:04 +0300

    feat: add hidden modal directive

---
+++ src/app/shared/directives/hidden-modal.spec.ts
@@ -0,0 +1,8 @@
+import { HiddenModal } from './hidden-modal';
+
+describe('HiddenModal', () => {
+  it('should create an instance', () => {
+    const directive = new HiddenModal();
+    expect(directive).toBeTruthy();
+  });
+});
