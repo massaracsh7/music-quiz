b60b5911d72c9569dd0c80e4d2e6bc5bfee8f670
massaracsh7 <fatum7@gmail.com>
2025-09-22 19:53:54 +0300

    feat: add 404 page

--- src/app/components/not-found-page/not-found-page.ts
+++ src/app/components/not-found-page/not-found-page.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-not-found-page',
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './not-found-page.html',
   styleUrl: './not-found-page.scss',
 })
