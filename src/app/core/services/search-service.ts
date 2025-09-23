2ff54480245f87df9394f09a09bf3eee901d31df
Jnuka <jnuka@mail.ru>
2025-09-23 21:40:16 +0300

    feat: add fun filter tracks

--- src/app/core/services/search-service.ts
+++ src/app/core/services/search-service.ts
@@ -1,6 +1,6 @@
 import { inject, Injectable } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { map, Observable } from 'rxjs';
 import { ITunesResponse, ITunesTrack } from '../../models/i-tunes.model';
 
 @Injectable({
@@ -9,7 +9,7 @@ import { ITunesResponse, ITunesTrack } from '../../models/i-tunes.model';
 export class SearchService {
   private http = inject(HttpClient);
 
-  public searchTracks(query: string, limit: number = 17): Observable<ITunesResponse> {
+  public searchTracksToItunes(query: string, limit: number = 17): Observable<ITunesResponse> {
     const parameters = new HttpParams()
       .set('term', query)
       .set('media', 'music')
@@ -20,5 +20,11 @@ export class SearchService {
 
   public filterOnlyTracks(tracks: ITunesTrack[]): ITunesTrack[] {
     return tracks.filter((track) => track.kind === 'song');
+  }
+
+  public searchTracks(query: string): Observable<ITunesTrack[]> {
+    return this.searchTracksToItunes(query).pipe(
+      map((response) => this.filterOnlyTracks(response.results)),
+    );
   }
 }
