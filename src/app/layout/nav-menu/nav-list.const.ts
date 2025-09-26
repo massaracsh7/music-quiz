3460dfc62bcfa6b47c74bcaa6562fb248fa1cf83
massaracsh7 <fatum7@gmail.com>
2025-09-26 21:37:22 +0300

    fix: fix router guards

--- src/app/layout/nav-menu/nav-list.const.ts
+++ src/app/layout/nav-menu/nav-list.const.ts
@@ -1,6 +1,5 @@
 export const NAV_LIST = [
   { path: '/home', label: 'NAV.HOME', icon: 'house' },
-  { path: '/game', label: 'NAV.GAME', icon: 'controller' },
   { path: '/leaderboard', label: 'NAV.LEADERBOARD', icon: 'trophy' },
   { path: '/about', label: 'NAV.ABOUT_US', icon: 'info-circle' },
   { path: '/categories', label: 'NAV.CATEGORY', icon: 'collection' },
