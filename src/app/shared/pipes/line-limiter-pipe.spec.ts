22277460682e8d391093fd9633c8ce8a57c0baca
Jnuka <jnuka@mail.ru>
2025-09-26 17:23:52 +0400

    refactor: remove unused pipe

--- src/app/shared/pipes/line-limiter-pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { LineLimiterPipe } from './line-limiter-pipe';
-
-describe('LineLimiterPipe', () => {
-  it('create an instance', () => {
-    const pipe = new LineLimiterPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
