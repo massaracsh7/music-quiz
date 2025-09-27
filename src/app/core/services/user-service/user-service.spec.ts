a98668a8b2f3be159a666fdd5fb9bac632dd7126
tubyliec <antikruk@vivaldi.net>
2025-09-27 22:38:24 +0300

    feat: add user-service test

--- src/app/core/services/user-service/user-service.spec.ts
+++ src/app/core/services/user-service/user-service.spec.ts
@@ -1,13 +1,30 @@
 import { TestBed } from '@angular/core/testing';
+import { UserService } from './user-service';
+import { Firestore } from '@angular/fire/firestore';
+import { Auth } from '@angular/fire/auth';
+import { TranslateService } from '@ngx-translate/core';
+import { ToastService } from '../../../shared/services/toast/toast';
 
-import { User } from './user';
+describe('UserService', () => {
+  let service: UserService;
 
-describe('User', () => {
-  let service: User;
+  const mockFirestore = jasmine.createSpyObj('Firestore', ['collection']);
+  const mockAuth = jasmine.createSpyObj('Auth', ['currentUser']);
+  const mockToastService = jasmine.createSpyObj('ToastService', ['showSuccess', 'showError']);
+  const mockTranslateService = jasmine.createSpyObj('TranslateService', ['instant']);
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(User);
+    TestBed.configureTestingModule({
+      providers: [
+        UserService,
+        { provide: Firestore, useValue: mockFirestore },
+        { provide: Auth, useValue: mockAuth },
+        { provide: ToastService, useValue: mockToastService },
+        { provide: TranslateService, useValue: mockTranslateService },
+      ],
+    });
+
+    service = TestBed.inject(UserService);
   });
 
   it('should be created', () => {
