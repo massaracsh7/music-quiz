6c831062a9b0f5e4c57e763264b7e624282ec7a9
tubyliec <antikruk@vivaldi.net>
2025-09-27 22:39:52 +0300

    feat: add ellipsis test

--- src/app/shared/directives/ellipsis/ellipsis.spec.ts
+++ src/app/shared/directives/ellipsis/ellipsis.spec.ts
@@ -1,8 +1,36 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { Component } from '@angular/core';
 import { Ellipsis } from './ellipsis';
+import { By } from '@angular/platform-browser';
+
+@Component({
+  template: '<div appEllipsis>Test content that should be truncated</div>',
+})
+class TestComponent {}
 
 describe('Ellipsis', () => {
+  let fixture: ComponentFixture<TestComponent>;
+  let component: TestComponent;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [TestComponent, Ellipsis],
+    });
+
+    fixture = TestBed.createComponent(TestComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
   it('should create an instance', () => {
-    const directive = new Ellipsis();
-    expect(directive).toBeTruthy();
+    const directiveEl = fixture.debugElement.query(By.directive(Ellipsis));
+    expect(directiveEl).toBeTruthy();
+  });
+
+  it('should apply ellipsis styles to the element', () => {
+    const div = fixture.debugElement.query(By.css('div')).nativeElement;
+    expect(div.style.overflow).toBe('hidden');
+    expect(div.style.whiteSpace).toBe('nowrap');
+    expect(div.style.textOverflow).toBe('ellipsis');
   });
 });
