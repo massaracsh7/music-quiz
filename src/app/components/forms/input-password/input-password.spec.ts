7ea74c746166cba635026bf9b7d89401ed43cdbe
tubyliec <antikruk@vivaldi.net>
2025-09-28 14:46:08 +0300

    feat: add input password test test

--- src/app/components/forms/input-password/input-password.spec.ts
+++ src/app/components/forms/input-password/input-password.spec.ts
@@ -1,18 +1,27 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { ReactiveFormsModule, FormControl } from '@angular/forms';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
 import { InputPassword } from './input-password';
+import { of } from 'rxjs';
 
 describe('InputPassword', () => {
   let component: InputPassword;
   let fixture: ComponentFixture<InputPassword>;
 
+  const mockTranslateService = {
+    instant: (key: string) => key,
+    get: (key: string) => of(key),
+  };
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [InputPassword],
+      imports: [InputPassword, ReactiveFormsModule, TranslateModule.forRoot()],
+      providers: [{ provide: TranslateService, useValue: mockTranslateService }],
     }).compileComponents();
 
     fixture = TestBed.createComponent(InputPassword);
     component = fixture.componentInstance;
+    component.control = new FormControl('');
     fixture.detectChanges();
   });
 
