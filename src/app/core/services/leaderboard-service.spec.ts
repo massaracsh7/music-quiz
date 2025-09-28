f54a63851f26f295049b1f4b6bf74e6d41d3fec2
tubyliec <antikruk@vivaldi.net>
2025-09-28 14:41:47 +0300

    feat: add leaderboard service test

--- src/app/core/services/leaderboard-service.spec.ts
+++ src/app/core/services/leaderboard-service.spec.ts
@@ -1,12 +1,36 @@
 import { TestBed } from '@angular/core/testing';
+import { Auth } from '@angular/fire/auth';
+import { Firestore } from '@angular/fire/firestore';
+import { of } from 'rxjs';
 
 import { LeaderboardService } from './leaderboard-service';
+import { AuthService } from './auth-service';
 
 describe('LeaderboardService', () => {
   let service: LeaderboardService;
+  let authService: jasmine.SpyObj<AuthService>;
+
+  const mockAuth = {
+    currentUser: Promise.resolve({ uid: 'test-uid' }),
+    onAuthStateChanged: () => () => {}
+  } as any;
+
+  const mockFirestore = {
+    collection: jasmine.createSpy('collection').and.returnValue({})
+  } as any;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    authService = jasmine.createSpyObj('AuthService', ['currentUser', 'isLoggedIn']);
+
+    TestBed.configureTestingModule({
+      providers: [
+        LeaderboardService,
+        { provide: Auth, useValue: mockAuth },
+        { provide: Firestore, useValue: mockFirestore },
+        { provide: AuthService, useValue: authService }
+      ]
+    });
+
     service = TestBed.inject(LeaderboardService);
   });
 
